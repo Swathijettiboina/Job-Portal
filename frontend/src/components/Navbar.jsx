f090d63636f7a4870274c662d6c36ab8045ab357
Swathijetti2004 <swathijettiboina@gmail.com>
2025-02-23 22:04:22 +0530

    job filters added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -45,6 +45,12 @@ const Navbar = () => {
             >
               Register
             </Link>
+            <Link
+              to="/about"
+              className="bg-gray-100 text-black px-4 py-2 rounded-md hover:bg-gray-200"
+            >
+              About
+            </Link>
           </div>
         </div>
       </div>
